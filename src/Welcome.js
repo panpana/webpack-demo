c396e6567a3fb74bce4925a8a1a2d111e73dc075
panpana <867159647@qq.com>
2018-03-04 20:56:53 +0800

    展示hook

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,10 +3,29 @@ class Welcome extends React.Component {
   constructor(props){
       super(props)
       this.state = {
-          date: new Date()
+        date: new Date(),
+        test: '1'
       }
+       setInterval(()=>{
+        this.setState({
+            date: new Date(),
+            test: 'constructor'
+        })
+     },5000)
+
+    console.log("我应经在 constructor 里将 props 和 state 初始化好了")
   }
+  conponentWillMount(){
+    this.setState({
+              date: new Date(), // 更新 date
+              test: 'componentWillMount'
+            })
+      console.log("运行到这里的话，说明马上就要运行 render 了~")
+  }
+
   render(){
+
+      console.log("嗯，这里是 render。")
       return (
           <div>
               <h1>Hello,{this.props.name}</h1>
@@ -14,6 +33,36 @@ class Welcome extends React.Component {
           </div>
       )
   }
+  componentDidMount(){
+    this.setState({
+              date: new Date(), // 更新 date
+              test: 'componentDidMount'
+            })
+      console.log("已经挂在到页面里啦")
+  }
+  componentWillReceiveProps(){
+      this.setState({
+          date: new Date(),
+          test: 'componentWillReceiveProps'
+      })
+  }
+  shouldComponentUpdate(){
+        // this.setState({
+        //     date: new Date(),
+        //     test: 'shouldComponentUpdate'
+        // })
+        return true
+    }  
+   componentWillUpdate(){
+       
+    }  
+   componentDidUpdate(){
+        
+    }  
+   componentWillUnmount(){
+    console.log('要死了')
+
+    }
 }
 
 export default Welcome 
