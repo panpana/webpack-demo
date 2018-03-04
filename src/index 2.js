1df8267e0a5ff2895d1d0a5a06472d4f6e6a6bf0
panpana <867159647@qq.com>
2018-03-04 17:13:47 +0800

    props

---
+++ src/index 2.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import $ from 'jquery'
+import foo from './foo'
+
+alert(1)
+
+function component () {
+    var element = $('<div></div>');
+  
+    /* lodash is required for the next line to work */
+    element.html(_.join(['Hello','webpack'], ' '))
+  
+    return element.get(0);
+  }
+  
+  document.body.appendChild(component());
+
+  console.log(foo)
+  console.log(foo())
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Welcome from './Welcome'
 
 ReactDOM.render(
-    <h1> Hi, World</h1>,
-    document.getElementById('root2')
-  );
+  <Welcome name="panpan"/>,
+  document.getElementById('root')
+)
