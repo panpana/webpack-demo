7735d6e5c3a58e3d3be107dee98e5947fdda3881
panpana <867159647@qq.com>
2018-03-05 22:44:16 +0800

    标记已完成/未完成

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     let todos = this.state.todoList.map((item,index)=>{
             return(
                       <li key={index}>
-                        <TodoItem todo={item} />
+                        <TodoItem todo={item} onToggle={this.toggle.bind(this)} />
                       </li>
             )
           })
@@ -36,6 +36,10 @@ class App extends Component {
 
 
     )
+  }
+  toggle(e, todo){
+      todo.status = todo.status === 'completed' ? '' : 'completed'
+      this.setState(this.state) 
   }
   changeTitle(event){
       this.setState({
