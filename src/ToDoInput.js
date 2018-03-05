913677db5ed6eb55565670c3d3f6c5f345261911
panpana <867159647@qq.com>
2018-03-05 22:29:08 +0800

    fix value agian

--- src/ToDoInput.js
+++ src/ToDoInput.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component{
     render(){
-        return <input type="text" defaultValue={this.props.content} 
-                onKeyPress={this.submit.bind(this)}/>
+        return <input type="text" value={this.props.content} 
+                onKeyPress={this.submit.bind(this)}
+                onChange={this.changeTitle.bind(this)}/>
     }
     submit(e){
             if (e.key === 'Enter') {
             this.props.onSubmit(e)
             console.log("按回车啦")
         }
+    }
+    changeTitle(e){
+          this.props.onChange(e)
     }
 }
