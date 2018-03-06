9aaf0136c710e103566575b1a3193a5b45ce0170
panpana <867159647@qq.com>
2018-03-06 10:54:14 +0800

    每次 setState 的时候 save，每次进页面的时候 load

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './reset.css'
 import 'normalize.css'
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
+import * as localStore from './localStore'
 
 class App extends Component {
     constructor(props){
         super(props)
         this.state = {
           newTodo: '',
-          todoList: []
+          todoList: localStore.load('todoList') || []
         }
       }
   render() {
@@ -43,16 +44,19 @@ class App extends Component {
   delete(event, todo){
       todo.deleted = true
       this.setState(this.state) 
+      localStore.save('todoList', this.state.todoList)
   }
   toggle(e, todo){
       todo.status = todo.status === 'completed' ? '' : 'completed'
       this.setState(this.state) 
+      localStore.save('todoList', this.state.todoList)
   }
   changeTitle(event){
       this.setState({
         newTodo: event.target.value,
         todoList: this.state.todoList
       })
+      localStore.save('todoList', this.state.todoList)
   }
   addTodo(event){
     this.state.todoList.push({
@@ -65,6 +69,7 @@ class App extends Component {
             newTodo: '',
             todoList: this.state.todoList
     })
+    localStore.save('todoList', this.state.todoList)
   }
 }
 
